6e38ffb05a8f0062492dfe74cf6a84b78f2c2a1f
MukeunKim <mukeunkim@gmail.com>
2020-01-24 00:07:26 +0900

    Remove 'thisInfo' in FiberScheduler

--- source/geod24/concurrency.d
+++ source/geod24/concurrency.d
@@ -241,22 +241,6 @@ class FiberScheduler
     }
 
     /**
-     * Returns an appropriate ThreadInfo instance.
-     *
-     * Returns a ThreadInfo instance specific to the calling Fiber if the
-     * Fiber was created by this dispatcher, otherwise it returns
-     * ThreadInfo.thisInfo.
-     */
-    @property ref ThreadInfo thisInfo() nothrow
-    {
-        auto f = cast(InfoFiber) Fiber.getThis();
-
-        if (f !is null)
-            return f.info;
-        return ThreadInfo.thisInfo;
-    }
-
-    /**
      * Returns a Condition analog that yields when wait or notify is called.
      *
      * Bug:
