817fe67cde2fe58c2a9c00adda40e540375769b9
MukeunKim <mukeunkim@gmail.com>
2020-01-24 18:25:41 +0900

    Apply coding style

--- source/geod24/concurrency.d
+++ source/geod24/concurrency.d
@@ -47,14 +47,13 @@ import core.thread;
  * Thrown on calls to `receive` if the thread that spawned the receiving
  * thread has terminated and no more messages exist.
  */
-class OwnerTerminated : Exception
+public class OwnerTerminated : Exception
 {
-    ///
-    this(string msg = "Owner terminated") @safe pure nothrow @nogc
+    /// Ctor
+    public this (string msg = "Owner terminated") @safe pure nothrow @nogc
     {
         super(msg);
     }
-
 }
 
 /**
@@ -64,7 +63,7 @@ class OwnerTerminated : Exception
  * with each logical thread.  It contains all implementation-level information
  * needed by the internal API.
  */
-struct ThreadInfo
+public struct ThreadInfo
 {
     /// Transceiver device required for message exchange between threads.
     public Transceiver     transceiver;
@@ -82,7 +81,7 @@ struct ThreadInfo
      * default instance when info is requested for a thread not created by the
      * Scheduler.
      */
-    static @property ref thisInfo() nothrow
+    public static @property ref thisInfo () nothrow
     {
         static ThreadInfo val;
         return val;
@@ -95,7 +94,7 @@ struct ThreadInfo
      * the messaging system for the thread and notifies interested parties of
      * the thread's termination.
      */
-    void cleanup()
+    public void cleanup ()
     {
     }
 }
@@ -133,13 +132,13 @@ public @property void thisScheduler (FiberScheduler value) nothrow
  * and may be instantiated and used, but is not a necessary part of the
  * default functioning of this module.
  */
-class ThreadScheduler
+public class ThreadScheduler
 {
     /**
      * This simply runs op directly, since no real scheduling is needed by
      * this approach.
      */
-    void start(void delegate() op)
+    public void start (void delegate () op)
     {
         op();
     }
@@ -147,7 +146,7 @@ class ThreadScheduler
     /**
      * Creates a new kernel thread and assigns it to run the supplied op.
      */
-    void spawn(void delegate() op)
+    public void spawn (void delegate () op)
     {
         auto t = new Thread({
             thisScheduler = new FiberScheduler();
@@ -163,7 +162,7 @@ class ThreadScheduler
      * Returns ThreadInfo.thisInfo, since it is a thread-local instance of
      * ThreadInfo, which is the correct behavior for this scheduler.
      */
-    @property ref ThreadInfo thisInfo() nothrow
+    public @property ref ThreadInfo thisInfo () nothrow
     {
         return ThreadInfo.thisInfo;
     }
@@ -171,7 +170,7 @@ class ThreadScheduler
     /**
      * Creates a new Condition variable.  No custom behavior is needed here.
      */
-    Condition newCondition(Mutex m) nothrow
+    public Condition newCondition (Mutex m) nothrow
     {
         return new Condition(m);
     }
@@ -189,7 +188,7 @@ public @property ref ThreadInfo thisInfo () nothrow
  * This is an example scheduler that creates a new Fiber per call to spawn
  * and multiplexes the execution of all fibers within the main thread.
  */
-class FiberScheduler
+public class FiberScheduler
 {
     private bool terminated;
     private bool dispatching;
@@ -198,7 +197,7 @@ class FiberScheduler
      * This creates a new Fiber for the supplied op and then starts the
      * dispatcher.
      */
-    void start(void delegate() op, size_t sz = 0)
+    public void start (void delegate () op, size_t sz = 0)
     {
         create(op, sz);
         dispatch();
@@ -207,7 +206,7 @@ class FiberScheduler
     /**
     * This commands the scheduler to shut down at the end of the program.
     */
-    void stop()
+    public void stop ()
     {
         terminated = true;
     }
@@ -217,7 +216,7 @@ class FiberScheduler
      * This created a new Fiber for the supplied op and adds it to the
      * dispatch list.
      */
-    void spawn(void delegate() op, size_t sz = 0) nothrow
+    public void spawn (void delegate() op, size_t sz = 0)
     {
         create(op, sz);
         FiberScheduler.yield();
@@ -227,7 +226,7 @@ class FiberScheduler
      * If the caller is a scheduled Fiber, this yields execution to another
      * scheduled Fiber.
      */
-    static void yield() nothrow
+    public static void yield () nothrow
     {
         // NOTE: It's possible that we should test whether the calling Fiber
         //       is an InfoFiber before yielding, but I think it's reasonable
@@ -243,19 +242,18 @@ class FiberScheduler
      * For the default implementation, `notifyAll`will behave like `notify`.
      *
      */
-    Condition newCondition() nothrow
+    public Condition newCondition () nothrow
     {
         return new FiberCondition();
     }
 
-protected:
     /**
      * Creates a new Fiber which calls the given delegate.
      *
      * Params:
      *   op = The delegate the fiber should call
      */
-    void create(void delegate() op, size_t sz = 0) nothrow
+    protected void create (void delegate() op, size_t sz = 0) nothrow
     {
         void wrap()
         {
@@ -271,7 +269,7 @@ protected:
     /**
      * Fiber which embeds a ThreadInfo
      */
-    static class InfoFiber : Fiber
+    static public class InfoFiber : Fiber
     {
 
         public this (void delegate () op) nothrow
@@ -285,15 +283,15 @@ protected:
         }
     }
 
-    protected class FiberCondition : Condition
+    public class FiberCondition : Condition
     {
-        this() nothrow
+        public this () nothrow
         {
             super(null);
             notified = false;
         }
 
-        override void wait() nothrow
+        public override void wait () nothrow
         {
             scope (exit) notified = false;
 
@@ -301,7 +299,7 @@ protected:
                 FiberScheduler.yield();
         }
 
-        override bool wait(Duration period) nothrow
+        public override bool wait (Duration period) nothrow
         {
             import core.time : MonoTime;
 
@@ -316,13 +314,13 @@ protected:
             return notified;
         }
 
-        override void notify() nothrow
+        public override void notify () nothrow
         {
             notified = true;
             FiberScheduler.yield();
         }
 
-        override void notifyAll() nothrow
+        public override void notifyAll () nothrow
         {
             notified = true;
             FiberScheduler.yield();
@@ -331,8 +329,7 @@ protected:
         private bool notified;
     }
 
-private:
-    void dispatch()
+    private void dispatch ()
     {
         import std.algorithm.mutation : remove;
 
@@ -370,9 +367,8 @@ private:
         this.dispatching = false;
     }
 
-private:
-    Fiber[] m_fibers;
-    size_t m_pos;
+    private Fiber[] m_fibers;
+    private size_t m_pos;
 }
 
 /*******************************************************************************
