58ec1d90fd86f72b6879587e0481064ff0dd882b
MukeunKim <mukeunkim@gmail.com>
2020-01-31 10:08:17 +0900

    Modify to exit in scheduler if 'OwnerTerminated' exception occurs

--- source/geod24/concurrency.d
+++ source/geod24/concurrency.d
@@ -1251,10 +1251,14 @@ private:
             synchronized (this.fibers_lock)
             {
                 auto t = m_fibers[m_pos].call(Fiber.Rethrow.no);
-                if (t !is null && !(cast(OwnerTerminated) t))
+                if (t !is null)
                 {
-                    throw t;
+                    if (cast(OwnerTerminated) t)
+                        break;
+                    else
+                        throw t;
                 }
+
                 if (m_fibers[m_pos].state == Fiber.State.TERM)
                 {
                     if (m_pos >= (m_fibers = remove(m_fibers, m_pos)).length)
