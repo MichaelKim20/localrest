2e30648a13a5a58ce099abd93255073ad75fd602
MukeunKim <mukeunkim@gmail.com>
2020-02-03 19:43:20 +0900

    Synchronize the `m_fibers` using the fiber_lock in FiberScheduler.create

--- source/geod24/concurrency.d
+++ source/geod24/concurrency.d
@@ -1031,6 +1031,8 @@ protected:
             op();
         }
 
+        this.fibers_lock.lock_nothrow();
+        scope (exit) this.fibers_lock.unlock_nothrow();
         m_fibers ~= new InfoFiber(&wrap);
     }
 
